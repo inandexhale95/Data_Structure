7f1d5e3d7a0e3abc9fb0d390faf5e51f4171cf7a
inandexhale <inandexhale@gmail.com>
2022-02-04 00:07:39 +0900

    deque.py

---
+++ deque.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+print(f"{'*' * 20} STACK {'*' * 20}")
+stack = deque()
+
+for i in range(1, 6):
+    stack.append(i)
+    print(stack)
+
+for i in range(5):
+    print(stack.pop())
+
+
+print(f"{'*' * 20} QUEUE {'*' * 20}")
+queue = deque()
+
+for i in range(1, 6):
+    queue.append(i)
+    print(queue)
+
+for i in range(5):
+    print(queue.popleft())
+
+
